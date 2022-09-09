c8d90f0e0440387c9a6d7741c8f2fb713c50c70a
yudc13 <290945266@qq.com>
2022-09-09 14:40:58 +0800

    feat: init

---
+++ src/app.config.ts
@@ -0,0 +1,30 @@
+export default defineAppConfig({
+  pages: [
+    'pages/course/index',
+    'pages/user/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black',
+  },
+  tabBar: {
+    color: '#999999',
+    selectedColor: '#04B8F1',
+    list: [
+      {
+        pagePath: 'pages/course/index',
+        text: '课程',
+        iconPath: 'static/tabBar/course.png',
+        selectedIconPath: 'static/tabBar/course-active.png',
+      },
+      {
+        pagePath: 'pages/user/index',
+        text: '我的',
+        iconPath: 'static/tabBar/user.png',
+        selectedIconPath: 'static/tabBar/user-active.png',
+      }
+    ]
+  }
+})
