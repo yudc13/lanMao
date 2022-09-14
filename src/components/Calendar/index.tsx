daf35597579e1e66db3fc8148a407a91913ed449
yudc13 <290945266@qq.com>
2022-09-14 15:06:46 +0800

    feat: 完善日历

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,21 +1,55 @@
 import {Text, View} from '@tarojs/components';
 import classNames from 'classnames'
+import dayjs from 'dayjs';
+import {useMemo, useState} from 'react';
+
 
 import {getCurrentCalendarDays, weeks} from './utils'
 
 import styles from './index.module.less'
 
+const today = dayjs().format('YYYY-MM-DD')
+
 const Calendar = () => {
-  const days = getCurrentCalendarDays()
-  console.log(days)
+  const [currentDate, setCurrentDate] = useState(today)
+  const days = useMemo(() => getCurrentCalendarDays(currentDate), [currentDate])
+  const handlePrevMonth = () => {
+    setCurrentDate(dayjs(currentDate).subtract(1, 'month').format('YYYY-MM-DD'))
+  }
+  const handleNextMonth = () => {
+    setCurrentDate(dayjs(currentDate).add(1, 'month').format('YYYY-MM-DD'))
+  }
+  const onTouchStart = (e) => {
+    console.log(e)
+  }
   return (
-    <View className={styles.calendar}>
-      <View className={styles.header}>Header</View>
-      <View className={styles.weeks}>
-        {weeks.map(week => <Text className={styles.week} key={week}>{week}</Text>)}
-      </View>
-      <View className={styles.days}>
-        {days.map(day => <Text className={classNames(styles.day, { [styles[day.type]]: true })} key={day.date}>{day.day}</Text>)}
+    <View className={styles.calendarContainer}>
+      <View className={styles.calendar}>
+        <View className={styles.calendarHeader}>
+          <Text onClick={handlePrevMonth}>上一月</Text>
+          <Text>{currentDate}</Text>
+          <Text onClick={handleNextMonth}>下一月</Text>
+        </View>
+        <View className={styles.calendarWeeks}>
+          {weeks.map(week => <Text className={styles.week} key={week}>{week}</Text>)}
+        </View>
+        <View className={styles.calendarPanel}>
+          {
+            Array.from({ length: 6 }).map((_, index) => (
+              <View className={styles.row} key={index}>
+                {days.slice(index * 7, index * 7 + 7).map(day => (
+                  <Text
+                    className={classNames(styles.day, { [styles[day.type]]: true, [styles.current]: day.date === today })}
+                    key={day.date}
+                  >
+                    {day.day}
+                  </Text>
+                ))}
+              </View>
+            ))
+          }
+        </View>
+        <View className={styles.controlBar} onTouchStart={onTouchStart}>拖动</View>
       </View>
     </View>
   )
--- src/components/Calendar/utils.ts
+++ src/components/Calendar/utils.ts
@@ -3,8 +3,8 @@ import dayjs from 'dayjs';
 export const weeks = ['日', '一', '二', '三', '四', '五', '六']
 
 // 获取当前月有多少天
-export const getDayCount = (year: number, month: number): number => {
-  return dayjs().set('year', year).set('month', month).daysInMonth()
+export const getDayCount = (date: string): number => {
+  return dayjs(date).daysInMonth()
 }
 
 // 获取当前月第一天是星期几(0~6)
@@ -13,9 +13,11 @@ export const getWeekDay = (year: number, month: number) => {
 }
 
 // 获取上一个月还有哪些天
-export const getPrevMonthRestDays = (year: number, month: number) => {
-  let lastMonthDays = getDayCount(year, month)
-  let weekDay = getWeekDay(year, month)
+export const getPrevMonthRestDays = (date: string) => {
+  // 上一个月
+  const prevDate = dayjs(date).subtract(1, 'month').format('YYYY-MM-DD')
+  let lastMonthDays = getDayCount(prevDate)
+  let weekDay = getWeekDay(dayjs(date).year(), dayjs(date).month())
   const restDays: number[] = []
   while (weekDay > 0) {
     restDays.push(lastMonthDays--)
@@ -25,8 +27,8 @@ export const getPrevMonthRestDays = (year: number, month: number) => {
 }
 
 // 获取当前月有哪些天
-export const getCurrentMonthDays = (year: number, month: number) => {
-  const dayCount = getDayCount(year, month)
+export const getCurrentMonthDays = (date: string) => {
+  const dayCount = getDayCount(date)
   const days: number[] = []
   for (let i = 1; i <= dayCount; i++) {
     days.push(i)
@@ -35,11 +37,11 @@ export const getCurrentMonthDays = (year: number, month: number) => {
 }
 
 // 获取下个月还有那些天
-export const getNextMonthRestDays = (year: number, month: number) => {
-  const dayCount = getDayCount(year, month - 1)
-  const weekDay = getWeekDay(year, month)
+export const getNextMonthRestDays = (date: string) => {
+  const dayCount = getDayCount(date)
+  const weekDay = getWeekDay(dayjs(date).year(), dayjs(date).month())
   const restDays: number[] = []
-  for (let i = 1; i <= (42 - dayCount - weekDay - 1); i++) {
+  for (let i = 1; i <= (42 - dayCount - weekDay); i++) {
     restDays.push(i)
   }
   return restDays
@@ -51,33 +53,31 @@ export const getCurrentCalendarDays = (date?: string) => {
   const prevDate = dayjs(date).subtract(1, 'month')
   const nextDate = dayjs(date).add(1, 'month')
 
-  const prevYear = prevDate.year()
-  const prevMonth = prevDate.month() + 1
-  const prevDays = getPrevMonthRestDays(prevYear, prevMonth)
-  const currentYear = current.year()
-  const currentMonth = current.month()
-  const currentDays = getCurrentMonthDays(currentYear, currentMonth)
-  const nextYear = nextDate.year()
-  const nextMonth = nextDate.month() + 1
-  const nextDays = getNextMonthRestDays(nextYear, nextMonth)
+  const currentStr = current.format('YYYY-MM')
+  const prevDateStr = prevDate.format('YYYY-MM')
+  const nextDateStr = nextDate.format('YYYY-MM')
+
+  const prevDays = getPrevMonthRestDays(currentStr)
+  const currentDays = getCurrentMonthDays(currentStr)
+  const nextDays = getNextMonthRestDays(currentStr)
   const calendarDays: { date: string, type: 'active' | 'disabled', day: number }[] = []
   prevDays.forEach(day => {
     calendarDays.push({
-      date: `${prevYear}-${prevMonth}-${day > 9 ? day : `0${day}`}`,
+      date: `${prevDateStr}-${day > 9 ? day : `0${day}`}`,
       type: 'disabled',
       day
     })
   })
   currentDays.forEach(day => {
     calendarDays.push({
-      date: `${currentYear}-${currentMonth}-${day > 9 ? day : `0${day}`}`,
+      date: `${currentStr}-${day > 9 ? day : `0${day}`}`,
       type: 'active',
       day
     })
   })
   nextDays.forEach(day => {
     calendarDays.push({
-      date: `${nextDays}-${nextMonth}-${day > 9 ? day : `0${day}`}`,
+      date: `${nextDateStr}-${day > 9 ? day : `0${day}`}`,
       type: 'disabled',
       day
     })
